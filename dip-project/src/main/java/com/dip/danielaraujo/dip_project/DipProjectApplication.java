09f43251064665e8b7f6b1c21452d5644ba656d3
Daniel Araújo <dannielaraujooficial@gmail.com>
2024-10-01 13:54:07 -0300

    concertando gerenciador de dependência

--- dip-project/src/main/java/com/dip/danielaraujo/dip_project/DipProjectApplication.java
+++ dip-project/src/main/java/com/dip/danielaraujo/dip_project/DipProjectApplication.java
@@ -1,13 +1,13 @@
-package com.dip.danielaraujo.demo;
+package com.dip.danielaraujo.dip_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DipProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DipProjectApplication.class, args);
 	}
 
 }
--- dip-project/src/test/java/com/dip/danielaraujo/dip_project/DipProjectApplicationTests.java
+++ dip-project/src/test/java/com/dip/danielaraujo/dip_project/DipProjectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dip.danielaraujo.demo;
+package com.dip.danielaraujo.dip_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DipProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
